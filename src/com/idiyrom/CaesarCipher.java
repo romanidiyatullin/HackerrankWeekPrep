170d8bf8eaf9cf38801b9558ec4a6c4f029edf3c
idiyrom <roman.idiyatullin@gmail.com>
2023-10-15 17:06:37 -0400

    Medium. Caesar Cipher. Full description: иhttps://www.hackerrank.com/challenges/caesar-cipher-1/problem

--- src/com/idiyrom/CaesarCipher.java
+++ src/com/idiyrom/CaesarCipher.java
@@ -72,7 +72,7 @@ public class CaesarCipher {
 }
 
 
-class Tests {
+class CaesarCipherTests {
 
     @Test
     public void elementaryTest(){
@@ -100,7 +100,6 @@ class Tests {
     public void complexTests(){
         String actual = "1X7T4VrCs23k4vv08D6yQ3S19G4rVP188M9ahuxB6j1tMGZs1m10ey7eUj62WV2exLT4C83zl7Q80M";
         String expected = "1Y7U4WsDt23l4ww08E6zR3T19H4sWQ188N9bivyC6k1uNHAt1n10fz7fVk62XW2fyMU4D83am7R80N";
-        int k = 27;
         Assertions.assertEquals(expected, CaesarCipher.caesarCipher(actual,27));
     }
 
